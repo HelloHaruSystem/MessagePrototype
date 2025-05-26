caa2c74eb34baa703550c107280f150fde43fac0
Halfdan <halfdansc@gmail.com>
2025-05-26 10:48:49 +0200

    cleaning up the ErrorMessage.java

--- src/main/java/com/example/MessageModels/ErrorMessage.java
+++ src/main/java/com/example/MessageModels/ErrorMessage.java
@@ -2,18 +2,35 @@ package com.example.MessageModels;
 
 public class ErrorMessage {
     private String code;
-        private String message;
-        private String details;
+    private String message;
+    private String details;
         
-        public ErrorMessage(String code, String message) {
-            this.code = code;
-            this.message = message;
-        }
+    public ErrorMessage(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
         
-        public String getCode() { return code; }
-        public void setCode(String code) { this.code = code; }
-        public String getMessage() { return message; }
-        public void setMessage(String message) { this.message = message; }
-        public String getDetails() { return details; }
-        public void setDetails(String details) { this.details = details; }
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
 }
